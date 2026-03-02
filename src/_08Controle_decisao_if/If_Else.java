01c178ed67593fd101abfeea76bed4d7522d0144
samuel <samuka.jvm@gmail.com>
2026-03-02 10:23:34 -0400

    If e Else em java

---
+++ src/_08Controle_decisao_if/If_Else.java
@@ -0,0 +1,32 @@
+package _08Controle_decisao_if;
+
+import java.util.Scanner;
+
+public class If_Else {
+    /* Curso de Java 14: Condicionais If-Else */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Entre com sua idade: ");
+        int idade = scan.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("É maior de idade.");
+        } else {
+            System.out.println("Não é maior de idade.");
+        }
+
+        System.out.print("Entre com o preço do item: ");
+        double valor = scan.nextDouble();
+
+        if (valor <= 10) {
+            System.out.println("Está barato, pode comprar");
+        } else if (valor > 10 && valor < 15) {
+            System.out.println("Você pode pedir um desconto");
+        } else if (valor >= 15 && valor < 17) {
+            System.out.println("Pode pesquisar mais");
+        } else {
+            System.out.println("Muito caro");
+        }
+    }
+}
