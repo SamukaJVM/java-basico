5587a424548b0b2a9d30b74e7ec0c6138825d44a
samuel <samuka.jvm@gmail.com>
2026-03-06 13:00:14 -0400

    For com Break em java

---
+++ src/_14Break/Break.java
@@ -0,0 +1,24 @@
+package _14Break;
+
+import java.util.Scanner;
+
+public class Break {
+    /* Curso de Java 18: Comandos Break e Continue */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Entre com um número: ");
+        int numero = scan.nextInt();
+
+        System.out.print("Entre com um limite: ");
+        int numeroMaximo = scan.nextInt();
+
+        for (int i = numero; i <= numeroMaximo; i++) {
+            System.out.println(i);
+            if (i % 7 == 0) {
+                System.out.println("O valor de i é: " + i);
+                break; // usado para sair de um for, assim que o valor for encontrado
+            }
+        }
+    }
+}
