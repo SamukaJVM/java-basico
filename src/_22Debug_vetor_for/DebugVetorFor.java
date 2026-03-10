b57c6d271c9c293fd9a057396e5406c17b2b6ec4
samuel <samuka.jvm@gmail.com>
2026-03-10 12:05:19 -0400

    Debug Vetor For em java

---
+++ src/_22Debug_vetor_for/DebugVetorFor.java
@@ -0,0 +1,13 @@
+package _22Debug_vetor_for;
+
+public class DebugVetorFor {
+    /* Curso de Java 22: Como debugar no Intellij/Eclipse */
+    public static void main(String[] args) {
+
+        int[] notas = new int[3];
+
+        for (int i=0; i<4; i++){
+            notas[i] = i;
+        }
+    }
+}
