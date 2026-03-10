df48c755cbd8b4446823859251a66c45ceb268ae
samuel <samuka.jvm@gmail.com>
2026-03-10 12:07:00 -0400

    Orientação a Objetos: Classes e atributos em java

---
+++ src/_24Classe_objeto_atributos/Carro.java
@@ -0,0 +1,10 @@
+package _24Classe_objeto_atributos;
+
+public class Carro {
+    /* declaração de atributos de classes */
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
