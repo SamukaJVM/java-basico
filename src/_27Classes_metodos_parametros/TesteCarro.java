105a7c26a9fef5dcdf1c7b93b52459b0c30b1822
samuel <samuka.jvm@gmail.com>
2026-03-18 10:22:21 -0400

    Orientação a Objetos: Classes e métodos com parâmetros em java

---
+++ src/_27Classes_metodos_parametros/TesteCarro.java
@@ -0,0 +1,29 @@
+package _27Classes_metodos_parametros;
+
+public class TesteCarro {
+    /* Curso de Java 27 - Orientação a Objetos: Classes e métodos com parâmetros */
+    public static void main(String[] args) {
+
+        Carro van = new Carro();
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        van.numPassageiros = 10;
+        van.capCombustivel = 100;
+        van.consumoCombustivel = 0.2;
+
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
+
+        van.exibirAutonomia();
+
+        double autonomia = van.obterAutonomia();
+        System.out.println("A autonomia do carro é: " + autonomia);
+        System.out.println("A autonomia do carro é: " + van.obterAutonomia());
+
+        double qtdCombustivel10 = van.calcularCombustivel(10);
+        double qtdCombustivel15 = van.calcularCombustivel(15);
+
+        System.out.println("qtdCombustivel10 = " + qtdCombustivel10);
+        System.out.println("qtdCombustivel15 = " + qtdCombustivel15);
+    }
+}
