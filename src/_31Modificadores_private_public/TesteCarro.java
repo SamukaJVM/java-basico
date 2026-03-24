e95a5a09a89470f71bf0c27d7712a1ffb3f32fef
samuel <samuka.jvm@gmail.com>
2026-03-24 08:56:38 -0400

    Orientação a Objetos: Modificadores private e public em java

---
+++ src/_31Modificadores_private_public/TesteCarro.java
@@ -0,0 +1,9 @@
+package _31Modificadores_private_public;
+
+public class TesteCarro {
+    /* Curso de Java 31 - Orientação a Objetos: Modificadores private e public */
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.marca = "Fiat";
+    }
+}
