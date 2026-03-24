5e0b9b7ab4c3e46853a11075aa7d273a29ddc509
samuel <samuka.jvm@gmail.com>
2026-03-24 08:56:13 -0400

    Orientação a Objetos: Palavra chave this em java

---
+++ src/_30Palavra_chave_this/TesteCarro.java
@@ -0,0 +1,9 @@
+package _30Palavra_chave_this;
+
+public class TesteCarro {
+    /* Curso de Java 30 - Orientação a Objetos: Palavra chave this */
+    public static void main(String[] args) {
+        Carro van = new Carro("Fiat", "Ducato");
+        System.out.println(van.numPassageiros);
+    }
+}
